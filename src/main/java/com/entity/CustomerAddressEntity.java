86670067b9e480a6806d2f2c7ec4472e0eecf479
Tejas Shah <tejasshah2k19@gmail.com>
2024-09-25 21:17:12 +0530

    1tom for cust and address

--- src/main/java/com/entity/CustomerAddressEntity.java
+++ src/main/java/com/entity/CustomerAddressEntity.java
@@ -1,37 +1,34 @@
 package com.entity;
 
-import java.util.List;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
 @Entity
-@Table(name = "customers")
+@Table(name = "customer_address")
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class CustomerEntity {
+public class CustomerAddressEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	Integer customerId;
-	String firstName;
-	String lastName;
-	String email;
-	String password;
-	String profilePicPath;
-	String otp;
-	String gender;
-	Integer bornYear;
-	String contactNum;
-
-	@OneToMany(mappedBy = "customer")
-	List<CustomerAddressEntity> address;
+	Integer addressId;
+	String title;
+	String addressLine;
+	Integer pincode;
+	Float lat;
+	Float log;
+	
+	@ManyToOne
+	@JoinColumn(name="customerId")
+	CustomerEntity customer;
+	
 	
 }
